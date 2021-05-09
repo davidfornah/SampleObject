2e8bd07f64282cacf08b66e08e42a37314076709
Dell <david.fornah.ubuntuafrika.gmail.com>
2021-05-09 02:11:22 +0000

    Initial commit Sapunka

---
+++ src/com/sapunka/Main.java
@@ -0,0 +1,12 @@
+package com.sapunka;
+public class Main {
+//    SAMPLE OBJECT PROGRAM THAT PRINT OUT AN INTEGER VALUE
+public static class MyClass{
+    int x = 5;
+    public static void main(String[] args) {
+	    MyClass myObj = new MyClass();
+        System.out.println("\nPrint Out the given value assigned to the variable");
+	    System.out.println(myObj.x);
+    }
+    }
+}
